793ca7b23fae74c37cd8325244b3b1163314845d
furkandindar <fdindar1@binghamton.edu>
2021-12-20 15:03:08 +0300

    add back to products button

--- src/components/BulgariRing.js
+++ src/components/BulgariRing.js
@@ -2,17 +2,22 @@ import React from 'react'
 import "@google/model-viewer/dist/model-viewer";
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import {useNavigate} from "react-router-dom";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 function BulgariRing() {
+    let navigate = useNavigate();
     return (
         <Grid
             container
             direction="column"
             justifyContent="center"
             alignItems="stretch"
-            spacing={10}
             >
-            <Grid item xs={2}></Grid>
+            <Grid item xs={2}>
+                <Button sx={{color: "#85715D"}} size="large" startIcon={<ArrowBackIosNewIcon/>} onClick={() => {navigate("/");}}>Products</Button>
+            </Grid>
             <Grid item xs={8}>
                 <Paper style={{height:"50vh", width:"100%"}}>
                     <model-viewer src="https://ttb-dev.s3.amazonaws.com/Bulgari_Ring_V2.glb" environment-image="neutral" ar ar-modes="webxr scene-viewer quick-look" seamless-poster shadow-intensity="1" camera-controls></model-viewer>
--- src/components/LVEarrings.js
+++ src/components/LVEarrings.js
@@ -2,17 +2,22 @@ import React from 'react'
 import "@google/model-viewer/dist/model-viewer";
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import {useNavigate} from "react-router-dom";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 function LVEarrings() {
+    let navigate = useNavigate();
     return (
         <Grid
             container
             direction="column"
             justifyContent="center"
             alignItems="stretch"
-            spacing={10}
             >
-            <Grid item xs={2}></Grid>
+            <Grid item xs={2}>
+                <Button sx={{color: "#85715D"}} size="large" startIcon={<ArrowBackIosNewIcon/>} onClick={() => {navigate("/");}}>Products</Button>
+            </Grid>
             <Grid item xs={8}>
                 <Paper style={{height:"50vh", width:"100%"}}>
                     <model-viewer src="https://ttb-dev.s3.amazonaws.com/LV_Earrings.glb" environment-image="neutral" ar ar-modes="webxr scene-viewer quick-look" seamless-poster shadow-intensity="1" camera-controls></model-viewer>
