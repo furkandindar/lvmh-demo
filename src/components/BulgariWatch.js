ab87261d3337e3c30413474170e2c2909ead07cb
furkandindar <fdindar1@binghamton.edu>
2021-12-23 16:39:30 +0300

    fix button margin

--- src/components/BulgariWatch.js
+++ src/components/BulgariWatch.js
@@ -245,9 +245,9 @@ function BulgariWatch() {
                 </Paper>
             </Grid>
             <Grid item container justifyContent="center"  xs={2}>
+                {button}
                 {isMobile ? <a onClick={viewInArButton} class="fancy-button bg-gradient1"><span><img className="seeinyourspace" src="./seeinyourspace.png"/>View in your space</span></a> : null}
                     <a id="arbutton" href="https://duz6y1s4uiy9h.cloudfront.net/Bvlgari_Web_V6.usdz" style={{display:"none"}} rel="ar"> <img /></a>
-                {button}
             </Grid>
         </Grid>
         </ThemeProvider>
